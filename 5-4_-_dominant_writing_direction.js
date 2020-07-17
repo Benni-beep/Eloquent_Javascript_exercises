8c700ff5bf64546216fc6fe8698aac47ff7e0e81
Benni-beep <krikelkr4kel@gmail.com>
2020-07-17 07:41:53 +0200

    added 5-4_dominant_writing_direction.js

---
+++ 5-4_-_dominant_writing_direction.js
@@ -0,0 +1,19 @@
+/**
+Dominant Writing Direction
+- input: string
+# - umwandeln in array -> split in Wörter? oder in zeichen?
+# - für jedes element des array überprüfen zu welcher sprache es gehört
+	-> charCodeAt(0)
+
+map 
+function direction(string){
+überprüfe charCodeAt(position)
+alle writing directions als variablen = 0
+dann +1 für writing direction variable zu der der charCode gehört
+}
+
+string.map()
+
+reducer = (a, b) => ???
+string.reduce(reducer)
+ */
