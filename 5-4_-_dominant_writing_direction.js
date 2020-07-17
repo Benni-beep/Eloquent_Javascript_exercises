09908c3417a63785a7c523be1e26af6985a8ef70
Benni-beep <krikelkr4kel@gmail.com>
2020-07-17 09:45:02 +0200

    finished dominant writing direction

--- 5-4_-_dominant_writing_direction.js
+++ 5-4_-_dominant_writing_direction.js
@@ -1,19 +1,51 @@
-/**
-Dominant Writing Direction
-- input: string
-# - umwandeln in array -> split in Wörter? oder in zeichen?
-# - für jedes element des array überprüfen zu welcher sprache es gehört
-	-> charCodeAt(0)
+"use strict"
+const SCRIPTS = require('./05_higher_order/code/scripts.js')
 
-map 
-function direction(string){
-überprüfe charCodeAt(position)
-alle writing directions als variablen = 0
-dann +1 für writing direction variable zu der der charCode gehört
+function characterScript(code) {
+	for (let script of SCRIPTS) {
+		if (script.ranges.some(([from, to]) => {
+			return code >= from && code < to;
+		})) {
+			return script;
+		}
+	}
+	return null;
 }
 
-string.map()
+function countBy(items, groupName) {
+	let counts = [];
+	for (let item of items) {
+		let name = groupName(item);
+		let known = counts.findIndex(c => c.name == name);
+		if (known == -1) {
+			counts.push({ name, count: 1 });
+		} else {
+			counts[known].count++;
+		}
+	}
+	return counts;
+}
 
-reducer = (a, b) => ???
-string.reduce(reducer)
- */
+function dominantDirection(str) {
+	const charCodes = str.split("").map(l => l.charCodeAt(0));
+
+	// 1. map determines corresponding script of each character code from array [charCodes] and returns whole scripts as objects in an array
+	// 2. filter removes any instance of 'null' from array of scripts object (resulting from " ", "!", ...)
+	// 3. map returns array with direction of script for each character
+	const directions = charCodes.map(char => characterScript(char)).filter(script => script !== null).map(script => script.direction)
+
+	const ltr = directions.filter(d => d === "ltr").length;
+	const rtl = directions.filter(d => d === "rtl").length;
+	const ttb = directions.filter(d => d === "ttb").length;
+
+	return ltr > rtl && ltr > ttb ?
+		"ltr" :
+		rtl > ltr && rtl > ttb ?
+			"rtl" :
+			"ttb"
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
